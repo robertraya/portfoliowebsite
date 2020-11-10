d75383b0cdcfeb80a9df32a9ed14a3c1dbd94629
robertraya <robertjamesraya@gmail.com>
2020-11-09 17:09:43 -0700

    Update urls.py

--- praisetheflesh/praisetheflesh/urls.py
+++ praisetheflesh/praisetheflesh/urls.py
@@ -19,5 +19,5 @@ from ptf import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.MainPage, name='mainpage'),
+    path('praisetheflesh.herokuapp.com/', views.MainPage, name='mainpage'),
 ]
