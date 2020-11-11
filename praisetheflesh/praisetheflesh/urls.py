2ab8c513ea8108b179e87e723294bbf4db04fb15
robertraya <robertjamesraya@gmail.com>
2020-11-10 18:15:01 -0700

    weho

--- praisetheflesh/praisetheflesh/urls.py
+++ praisetheflesh/praisetheflesh/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('praisetheflesh.herokuapp.com', views.MainPage, name='mainpage'),
+    path('praisethfeflesh', views.MainPage, name='mainpage'),
 ]
