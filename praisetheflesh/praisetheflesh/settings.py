b06a33b18ec302fb884b859af9b61bde31d92839
robertraya <robertjamesraya@gmail.com>
2020-11-10 19:51:55 -0700

    Update settings.py

--- praisetheflesh/praisetheflesh/settings.py
+++ praisetheflesh/praisetheflesh/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = '.urls'
+ROOT_URLCONF = 'praisetheflesh/praisetheflesh.urls'
 
 TEMPLATES = [
     {
