3cb4598126d1e94b63e02c4ce604299d0892008b
robertraya <robertjamesraya@gmail.com>
2020-11-10 20:16:54 -0700

    Update settings.py

--- praisetheflesh/praisetheflesh/settings.py
+++ praisetheflesh/praisetheflesh/settings.py
@@ -144,3 +144,19 @@ SECURE_SSL_REDIRECT = False
 
 #Activate Django-Heroku
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
