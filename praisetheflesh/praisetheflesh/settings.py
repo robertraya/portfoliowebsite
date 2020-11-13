b8ec2555432cb24629924afb3b99d605f035dd05
robertraya <robertjamesraya@gmail.com>
2020-11-13 13:19:37 -0700

    Update settings.py

--- praisetheflesh/praisetheflesh/settings.py
+++ praisetheflesh/praisetheflesh/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('praisetheflesh.secret_key.txt') as f:
+with open('/praisetheflesh/praisetheflesh/secret_key.txt') as f:
 	SECRET_KEY = f.read().strip()
 
 
