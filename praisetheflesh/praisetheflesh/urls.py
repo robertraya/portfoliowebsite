77534415cc2d8a2a7e55ff6d86418c30d789054d
robertraya <robertjamesraya@gmail.com>
2020-11-22 10:47:57 -0700

    runnin this just so i can make migrations

--- praisetheflesh/praisetheflesh/urls.py
+++ praisetheflesh/praisetheflesh/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from praisetheflesh.ptf import views
+from ptf import views
 
 urlpatterns = [
     path('youdontwannabehere/', admin.site.urls),
